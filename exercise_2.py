55f8edad6f349f54e294007004e62862ba50327f
Sipho Mabizela <simabjhb025@student.wethinkcode.co.za>
2026-02-19 18:05:47 +0200

    File name changed from nested dictionaries to exercise_2.

---
+++ exercise_2.py
@@ -0,0 +1,13 @@
+# NESTED DICTIONARIES
+
+# Structure: Dictionary -> List -> Dictionary
+
+volunteer_dict = {
+    '2024-02-19': [{
+        'Volunteer Name': 'Sipho',
+        'Status': 'Available',
+        'Time Start': '08:00',
+        'Time End': '10:00'
+    }]
+}
+
--- nested_dictionaries.py
+++
@@ -1,3 +0,0 @@
-# Structure: Dictionary -> List -> Dictionary
-
-volunteer_dict = {}
